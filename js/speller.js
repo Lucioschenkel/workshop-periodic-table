287568407131046b3e398592901bb61f4cabe541
Lucioschenkel <lucio.schenkel@proton.me>
2025-04-29 14:59:13 -0300

    feat: first implementation

--- js/speller.js
+++ js/speller.js
@@ -1,9 +1,10 @@
 export default {
-	check,
-	lookup,
+  check,
+  lookup,
 };
 
 var elements;
+var elementsMap;
 
 await loadPeriodicTable();
 
@@ -11,20 +12,59 @@ await loadPeriodicTable();
 // ****************************
 
 async function loadPeriodicTable() {
-	elements = await (await fetch("periodic-table.json")).json();
+  elements = await (await fetch("periodic-table.json")).json();
+  elementsMap = elements.reduce((map, currentElement, index) => {
+    map.set(currentElement.symbol.toLowerCase(), index);
+
+    return map;
+  }, new Map());
 }
 
 function check(inputWord) {
-	// TODO: determine if `inputWord` can be spelled
-	// with periodic table symbols; return array with
-	// them if so (empty array otherwise)
+  if (inputWord.length < 1) {
+    return [];
+  }
 
-	return [];
+  // check for two letter candidates
+  if (inputWord.length >= 2) {
+    const two = inputWord.substring(0, 2);
+
+    if (elementsMap.has(two)) {
+      if (two === inputWord) {
+        return [two];
+      }
+
+      const rest = check(inputWord.substring(2));
+
+      if (rest.length > 0) {
+        return [two, ...rest];
+      }
+    }
+  }
+
+  // check for one letter candidates
+  const one = inputWord.substring(0, 1);
+
+  if (elementsMap.has(one)) {
+    if (one === inputWord) {
+      return [one];
+    }
+
+    const rest = check(inputWord.substring(1));
+
+    if (rest.length > 0) {
+      return [one, ...rest];
+    }
+  }
+
+  return [];
 }
 
 function lookup(elementSymbol) {
-	// TODO: return the element entry based on specified
-	// symbol (case-insensitive)
+  const lowerCaseSymbol = elementSymbol.toLowerCase();
+  if (elementsMap.has(lowerCaseSymbol)) {
+    return elements[elementsMap.get(lowerCaseSymbol)];
+  }
 
-	return {};
+  return {};
 }
